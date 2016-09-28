4d3b021692386c9da4d46ef756a3b97274256fbf
Justin Ouellette <ouellette.justin@gmail.com>
2016-09-28 16:29:45 -0400

    procfile

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//config
+var token = '6UmmrF9HuA5xByLU1Giokjwq';
+
 //requires
 var express = require('express'),
     bodyParser = require('body-parser');
@@ -7,9 +10,16 @@ var app = express();
 
 //initialization
 app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
 
 //routes
 app.get('/', function (req, res) {
   res.json({"message": "hi!"});
 });
 
+
+//run
+var server = app.listen(app.get('port'), function () {
+  var port = server.address().port;
+  console.log('slashmtg app listening on %s', port);
+});
